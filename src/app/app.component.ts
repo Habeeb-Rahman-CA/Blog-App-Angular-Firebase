2e767b14320d726c7b619f06d6ab9f3a9529e00f
HabeebR07 <habeebrahmanca07@gmail.com>
2024-11-19 09:01:30 +0530

    added post card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { CategoryNavbarComponent } from './layout/category-navbar/category-navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { PostCardComponent } from './layout/post-card/post-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, PostCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
