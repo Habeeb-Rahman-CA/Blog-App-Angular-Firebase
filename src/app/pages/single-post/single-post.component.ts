90489f5dcf93aa520127d8fff80b394382b26a1f
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2024-11-23 17:02:40 +0530

    added comment section and comment list

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from "../../layout/post-card/post-card.component";
+import { CommentFormComponent } from "../../comments/comment-form/comment-form.component";
+import { CommentListComponent } from "../../comments/comment-list/comment-list.component";
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [PostCardComponent],
+  imports: [PostCardComponent, CommentFormComponent, CommentListComponent],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })
